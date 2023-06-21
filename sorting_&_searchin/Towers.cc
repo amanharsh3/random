0f705f7c00bbdc666f48472a98fddc5a898ea0ef
amanharsh3 <97787149+amanharsh3@users.noreply.github.com>
2023-06-22 00:45:50 +0530

    Towers.cc

---
+++ sorting_&_searchin/Towers.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;   
+typedef unsigned long long ull; 
+#define endl "\n"
+#define pb push_back
+#define INF 1e18
+#define all(x) begin(x), end(x)
+#define print(a) for(auto  &i: a) cout<<i<<" ";
+const int mod = 1e9 + 7;
+// cout << fixed << setprecision(0) << pi << endl;
+ 
+//debug statements
+#define debug1(p)        cout << #p << ": " << p << endl;
+#define debug2(p, q)     cout << #p << ": " << p << " | " << #q << ": " << q << endl;
+#define debug3(p, q, r)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << endl;
+#define debug4(p, q, r, s)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << " | "<< #s << ": " << s << endl;
+#define debug5(p, q, r, s, t)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << " | "<< #s << ": " << s <<" | "<< #t << ": " << t << endl;
+ 
+// check if test cases are mentioned or not
+void solve() {
+    int n;
+    cin>>n;
+ 
+    multiset<ll> st;
+    for(int i=0; i<n; i++) {
+    	ll x; cin>>x;
+    	auto it = st.upper_bound(x);
+    	if(it == st.end()) {
+    		st.insert(x);
+    	}
+    	else {
+    		st.erase(it);
+    		st.insert(x);
+    	}
+    }
+    cout<<st.size()<<endl;
+}
+ 
+int32_t main() {    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+    int t=1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
