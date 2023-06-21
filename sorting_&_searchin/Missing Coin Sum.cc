a5bf3b00cc4b08f8291c479803822a69dd596104
amanharsh3 <97787149+amanharsh3@users.noreply.github.com>
2023-06-22 00:41:18 +0530

    Missing Coin Sum.cc

---
+++ sorting_&_searchin/Missing Coin Sum.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;   
+typedef unsigned long long ull; 
+#define endl "\n"
+#define pb push_back
+#define INF 1e18
+#define all(x) begin(x), end(x)
+#define print(a) for(auto  &i: a) cout<<i<<" ";
+const int mod = 1e9 + 7;
+// cout << fixed << setprecision(0) << pi << endl;
+ 
+//debug statements
+#define debug1(p)        cout << #p << ": " << p << endl;
+#define debug2(p, q)     cout << #p << ": " << p << " | " << #q << ": " << q << endl;
+#define debug3(p, q, r)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << endl;
+#define debug4(p, q, r, s)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << " | "<< #s << ": " << s << endl;
+#define debug5(p, q, r, s, t)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << " | "<< #s << ": " << s <<" | "<< #t << ": " << t << endl;
+ 
+// check if test cases are mentioned or not
+void solve() {
+    int n;
+    cin>>n;
+ 
+    vector<ll> a(n); 
+    for(auto &i: a) cin>>i;
+   	sort(all(a));
+ 
+    ll currSum = 0;
+	for(int i=0; i<n; i++) {
+		if(currSum + 1 < a[i]) break;
+		currSum += a[i];
+	}
+	cout<<currSum+1<<endl;
+}
+ 
+int32_t main() {    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+    int t=1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
