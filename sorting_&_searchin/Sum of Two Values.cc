759b512fd8f903b2e1ac03f62fdba203916bda18
amanharsh3 <97787149+amanharsh3@users.noreply.github.com>
2023-06-22 00:38:23 +0530

    Sum of Two Values.cc

---
+++ sorting_&_searchin/Sum of Two Values.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;   
+typedef unsigned long long ull; 
+#define endl "\n"
+#define pb push_back
+#define INF 1e18
+#define all(x) begin(x), end(x)
+#define print(a) for(auto  &i: a) cout<<i<<" ";
+const int mod = 1e9 + 7;
+// cout << fixed << setprecision(0) << pi << endl;
+ 
+//debug statements
+#define debug1(p)        cout << #p << ": " << p << endl;
+#define debug2(p, q)     cout << #p << ": " << p << " | " << #q << ": " << q << endl;
+#define debug3(p, q, r)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << endl;
+#define debug4(p, q, r, s)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << " | "<< #s << ": " << s << endl;
+#define debug5(p, q, r, s, t)  cout << #p << ": " << p << " | " << #q << ": " << q << " | " << #r << ": " << r << " | "<< #s << ": " << s <<" | "<< #t << ": " << t << endl;
+ 
+// check if test cases are mentioned or not
+void solve() {
+    int n, k;
+    cin>>n>>k;
+ 
+    vector<ll> a(n+1);
+    map<ll, ll> mp;
+    for(int i=1; i<=n; i++) {
+    	cin>>a[i];
+    	mp[a[i]] = i;
+    }
+ 
+    for(int i=1; i<=n; i++) {
+    	if(mp.find(k - a[i]) != mp.end() && i != mp[k-a[i]]) {
+    		cout<<i<<" "<<mp[k-a[i]]<<endl; 
+    		return;
+    	}
+    }
+    cout<<"IMPOSSIBLE"<<endl;
+}
+ 
+int32_t main() {    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+    int t=1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
